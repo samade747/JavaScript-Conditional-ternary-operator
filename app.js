f0cf3dd950a3eaf2f94f1d6209ea60e742818dff
samade747 <samad.e747@gmail.com>
2023-10-29 23:29:30 +0500

    322353

---
+++ app.js
@@ -0,0 +1,14 @@
+// ternary operator 
+
+
+// old style
+// var a =5 
+// if(a == 5){
+//     console.log('han Ok')
+// } else{
+//     console.log('Not Ok')
+// }
+
+
+// ternary operator 
+var a = (5 == 5 ? 'han Ok' : 'nikal')
